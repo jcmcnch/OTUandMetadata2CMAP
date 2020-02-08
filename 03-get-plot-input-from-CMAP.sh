86441bcd05f33c80fb64eb92c1aa616ff1d8f10a
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2020-02-07 16:18:31 -0800

    fix loop structure

--- 03-get-plot-input-from-CMAP.sh
+++ 03-get-plot-input-from-CMAP.sh
@@ -6,13 +6,14 @@ for hash in `cat hashes/* | cut -f1`; do
 
 	if ls filtered/*/*$hash*png 1> /dev/null 2>&1; then
 
-                echo "Output already exists for $hash. Delete the output files if you want to rerun."
-                mv filtered/$hash* filtered/finished
+                echo "Output graph does not exist yet for $hash. Filtering CMAP data to correct formatting and remove depths that will cause problems for interpolated plots."
+                dataset-specific-filtering-GA03.sh $hash filtered
 
 	else
 
-                echo "Output graph does not exist yet for $hash. Filtering CMAP data to correct formatting and remove depths that will cause problems for interpolated plots."
-                dataset-specific-filtering.sh $hash filtered
+                echo "Output already exists for $hash. Delete the output files if you want to rerun."
+                mv filtered/$hash* filtered/finished
+
 
         fi
 
